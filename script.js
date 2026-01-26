6e24ec747a1df37608206ca9c7bdd7871db70a1f
a-guigue <aurelien.guigue.pro@gmail.com>
2026-01-26 11:12:23 +0100

    modif css + modif du js

--- script.js
+++ script.js
@@ -38,18 +38,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
-
-let lastScrollTop = 0;
-const nav = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
-    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScroll > lastScrollTop && currentScroll > 100) {
-        nav.classList.add('hidden');
-    } else {
-        nav.classList.remove('hidden');
-    }
-
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-});
